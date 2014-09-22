c582eb5707eaa39e6ea4870db3a23a17aac2015b
Hubert Nafalski <Hubert.Nafalski@gmail.com>
2014-09-22 21:46:14 +0200

    run jasper in debug mode
    by default runs with local mic and no speakers (just prints out phrase);
    optional command line parameter --log-to-console  prints out all debug
    messages to console

---
+++ client/debug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ -z "$JASPER_HOME" ]]; then
+    if [[ -d "/home/pi" ]]; then
+        JASPER_HOME="/home/pi"
+        export JASPER_HOME;
+    else
+        echo "Error: \$JASPER_HOME is not set."
+        exit 0;
+    fi
+fi
+
+cd $JASPER_HOME/jasper/client
+rm -rf ../old_client
+python main.py -l -n
