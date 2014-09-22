fdd7487b60e10e29efcb369c5a1752ddd88f2414
Hubert Nafalski <Hubert.Nafalski@gmail.com>
2014-09-22 22:40:56 +0200

    added use of audio files cache and phares spliting
    audio files synthesised by google api are being cached to spare network
    bandwidth and time. Phrases are being splited in smaller parts to make
    better use of cache:
    - when phrase contains | it will be splited exactly here
    - when phrease contains ~ it won't be cached at all
    - when phrase is longer than 30 characters (google api won't accep it)
    it will be splited by one of given chars: . , : ; new line.
    Example:
    phrase 'Now it is|~ 8:45 AM' will be splited in two:
    'Now it is' - will be cached for futher use
    '8:45 AM' - won't be cached at all since it's unlikely to be used
    frequently
    TODO:
    - cache control sings need to be hardcoded in each module and come core
    classes - check what influance it has on espaek engine

--- client/speaker.py
+++ client/speaker.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 A Speaker handles audio output from Jasper to the user
@@ -8,30 +7,93 @@ Speaker methods:
     play - play the audio in 'filename'
     isAvailable - returns True if the platform supports this implementation
 """
+# może teraz?
 import os
+import re
 import json
+import shelve
+import time
+import datetime
+import str_formater
+import subprocess
+import persistent_cache
+
+PHRASES_CACHE_DB    = 'cache_phrases.db'
 
 
 class GoogleSpeaker:
 
     """
     Uses the google speech synthesizer - requires to be on-line
-    """
+    """ 
     def __init__(self, logger):
         self.logger = logger
+        self.cache = persistent_cache.audioCache(PHRASES_CACHE_DB, logger)
+        self.cache.listCacheEntries()
 
     @classmethod
     def isAvailable(cls):
         return True
 
-    def say(self, phrase):
-        cmd = "wget -q --restrict-file-names=nocontrol --referer=\"http://translate.google.com\" -U Mozilla -O say.mp3 \"http://translate.google.com/translate_tts?ie=UTF-8&tl=pl&q=%s\" && avconv -y -v quiet -i say.mp3 -f wav say.wav" % phrase
-        self.logger.debug("command for google translator: %s" % cmd)
-        os.system(cmd)
-        self.play("say.wav")
 
-    def play(self, filename):
-        os.system("aplay -D hw:2,0 " + filename)
+    def useTTS(self, phrase):
+        try:
+          #cmd = "wget -q --restrict-file-names=nocontrol --referer=\"http://translate.google.com\" -U Mozilla -O say.mp3 \"http://translate.google.com/translate_tts?ie=UTF-8&tl=pl&q=%s\" && avconv -y -v quiet -i say.mp3 -f wav say.wav" % phrase
+          self.logger.debug("command for google translator: %s" % phrase)
+          phrase = str_formater.unicodeToUTF8(phrase, self.logger)
+          cmd = ["/home/pi/jasper/client/tts.sh", phrase]
+          subprocess.call(cmd)
+          audio_file = "say.wav"
+        except:
+          self.logger.critical("fatal error using google tts", exc_info=True)
+          audio_file = None
+        
+        return audio_file
+
+    def say(self, phrase):
+        #phrase = str_formater.utf8ToUnicode(phrase, self.logger)
+        if "|" in phrase:
+            #phrases = re.compile(u'\. |, |: |; |\n', re.UNICODE).split(phrase)
+            phrases = re.compile('\|').split(phrase)
+        elif len(phrase) > 30 :
+            phrases = re.compile('\. |, |: |; |\n').split(phrase)
+        else:
+            phrases = [phrase]
+        
+        audio_sentance = ""
+        index = 0
+        for p in phrases:
+            self.logger.debug("JASPER: " + p)
+            audio_file = None
+            #self.speaker.say(p)
+            #time.sleep(1)
+
+            #p = str_formater.unicodeToUTF8(p, self.logger)
+            # ~ means do not cache - used when prases are unlikely to reacure in exact forme, for example current time
+            cache = True
+            if '~' in p:
+              cache = False
+              p = p.replace('~','')
+            if self.cache.hasKey(p) and cache:
+              audio_file = self.cache.getFromCache(p)
+            else:
+              audio_file = self.useTTS(p)
+              
+              if audio_file: 
+                if cache:
+                  audio_file = self.cache.addToCache(p, audio_file)
+                else:
+                  index += 1
+                  tmp_file = "tmp_%04d.wav" % (index)
+                  os.system("mv %s %s" % (audio_file, tmp_file))
+                  audio_file = tmp_file
+            audio_sentance += ' ' + audio_file
+            self.logger.debug('audio_sentance %s' % audio_sentance )
+                  
+        self.play(audio_sentance)
+
+    def play(self, filename):      
+        os.system("aplay -D hw:1,0 " + filename)
 
 class DummySpeaker:
 
@@ -69,7 +131,7 @@ class eSpeakSpeaker:
         self.play("say.wav")
 
     def play(self, filename):
-        os.system("aplay -D hw:2,0 " + filename)
+        os.system("aplay -D hw:1,0 " + filename)
 
 
 class saySpeaker:
